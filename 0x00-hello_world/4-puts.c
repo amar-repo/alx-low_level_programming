a58090dc234a8503401409dabf92e8756dc6f58b
amar-repo <umarahmed072@gmail.com>
2022-09-08 17:49:48 -0700

    Prints a string of charcters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /* First program to try the puts function */
 int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
+	puts ("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 }
 
