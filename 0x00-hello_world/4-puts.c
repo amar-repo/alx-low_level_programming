42db484f088a5d53a3a3186eea3a74e1490aaf33
amar-repo <umarahmed072@gmail.com>
2022-09-09 02:12:17 -0700

    Prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-/* First program to try the puts function */
+/** 
+ * Main - Entry point
+ * Return- 0 if exited properly
+ */
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
 
