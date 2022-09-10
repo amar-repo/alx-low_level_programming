05df1950c555c9ea4b9e6dea62fe96360bf10d22
amar-repo <umarahmed072@gmail.com>
2022-09-09 21:59:02 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main starts here
+ * Return: returns success 0
+ */
+int main(void)
+{
+	char alpha;
+	char BET;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (BET = 'A'; alpha <= 'Z'; BET++)
+	{
+		putchar(BET);
+	}
+		putchar('\n');
+		return (0);
+}
