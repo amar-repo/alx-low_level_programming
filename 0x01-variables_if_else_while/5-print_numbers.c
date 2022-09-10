86b3e576a7da657d40e770c7c3ea9d685fd6fbfe
amar-repo <umarahmed072@gmail.com>
2022-09-09 22:23:41 -0700

    Prints numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main starts here
+ * Return: program exits here
+ */
+int main(void)
+{
+	int num = 1;
+
+	for (num = 1; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
