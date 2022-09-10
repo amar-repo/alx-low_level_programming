34ec9be6d84d3d475fe8ba325aedabd59cefc8ac
amar-repo <umarahmed072@gmail.com>
2022-09-09 22:30:47 -0700

    Prints numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - starts here
+ * Return: ends here
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
