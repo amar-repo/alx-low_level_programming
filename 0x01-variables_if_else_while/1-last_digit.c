e483a3eb9a75bf4e16856a5c7b15154cc1217ea5
amar-repo <umarahmed072@gmail.com>
2022-09-09 20:15:03 -0700

    Prints info about random numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	if (rem == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n");
+		printf("Last digit of %d is %d and is 0\n", n, rem);
 	}
 	if (rem < 6 != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
 	}
 	return (0);
 }
