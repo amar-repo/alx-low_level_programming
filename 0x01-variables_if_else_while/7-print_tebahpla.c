0c95b7c190c55628889c9ff990cbc7da807e2732
amar-repo <umarahmed072@gmail.com>
2022-09-10 08:54:07 -0700

    Prints alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program start
+ * Return: program end
+ */
+int main(void)
+{
+	char hpla = 'z';
+
+	for (hpla = 'z'; hpla <= 'a'; hpla--)
+	{
+		putchar(hpla);
+	}
+	putchar('\n');
+}
