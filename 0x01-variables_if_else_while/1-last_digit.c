d64003641ec1ece32d9ea9c02aff3960b301f3b2
amar-repo <umarahmed072@gmail.com>
2022-09-09 20:12:18 -0700

    Prints info on random numbers

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Program start
+ * Return: Program returns 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int rem;
+
+	rem = n % 10;
+	if (rem > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
+	}
+	if (rem == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n");
+	}
+	if (rem < 6 != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n");
+	}
+	return (0);
+}
