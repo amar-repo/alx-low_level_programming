5cf4ad6c9a8b9b043df054d255d24a113cd0d80a
amar-repo <umarahmed072@gmail.com>
2022-09-11 14:51:43 -0700

    Prints possible combinations of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,19 +6,12 @@
 int main(void)
 {
 	int a;
-	int b;
 
-	for (a = 0; a <= 8; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = a + 1; b <= 9; b++)
-		{
-			putchar(a + '0');
-			putchar(b + '0');
-			if (a == 8 && b == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(a + '0');
+		putchar(',');
+		putchar(' ');
 	}
 	return (0);
 }
