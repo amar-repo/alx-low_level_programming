a55c257b9ce7f1291537351ff4ee11ecb0de3772
amar-repo <umarahmed072@gmail.com>
2022-09-11 15:21:32 -0700

    Prints digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
+		putchar(a + '0');
 		if (a == 9)
 		{
-			putchar(a + '0');
-			return (0);
+			continue;
 		}
-		putchar(a + '0');
 		putchar(',');
 		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
