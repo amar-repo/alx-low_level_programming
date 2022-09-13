015d7514c5d8f3e06d5e02da33c3c7eea653fc41
amar-repo <umarahmed072@gmail.com>
2022-09-13 14:39:35 -0700

    Prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Program start here
+ * Return: Program exits 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
