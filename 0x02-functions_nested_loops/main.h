e72daa79040fcc6efd216629a753fb1b2454ca63
amar-repo <umarahmed072@gmail.com>
2022-09-14 15:40:00 -0700

    Header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,9 @@
  * function prototypes
  */
 
-int _putchar(char)
-void print_alphabet(void)
-	
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+
 #endif
