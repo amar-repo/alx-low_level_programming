78fad75de097e6cfd55e956890be6933bb0d950c
amar-repo <umarahmed072@gmail.com>
2022-09-14 16:21:13 -0700

    Displays lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks for lower case
+ *
+ * Return: Program exits 0
+ */
+int _islower(int c)
+{
+	int a;
+
+	a = _islower('D');
+	_putchar(a + '0');
+	a = _islower('g');
+	_putchar(a + '0');
+	_putchar('\n');
+	return (0);
+}
