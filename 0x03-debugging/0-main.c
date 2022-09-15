b6ec39c9a5440984731ddffd2f42e0c1c726d72a
amar-repo <umarahmed072@gmail.com>
2022-09-15 08:47:14 -0700

    Checks for positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - Tests for positive or negative
+ *
+ * Return: Always 0
+ */
+void positive_or_negative(int i);
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}	
