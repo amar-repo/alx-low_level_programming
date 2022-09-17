2f7bbba85a3148f397d11590af4605b8571adbd6
amar-repo <umarahmed072@gmail.com>
2022-09-16 21:05:36 -0700

    Prints _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Prints line on terminal
+ * @n: number of times character is printed
+ *
+ * Return: Returns nothing
+ */
+void print_line(int n)
+{
+	int n;
+
+	while (n-- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
