1a5d21c7ef7a4472767aed4258dd5c35784cf874
amar-repo <umarahmed072@gmail.com>
2022-09-20 20:58:35 +0000

    Prints and reverses string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be reversed
+ * Return: Returns nothing
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+	{
+		_putchar(*(s + len));
+	}
+	_putchar('\n');
+}
