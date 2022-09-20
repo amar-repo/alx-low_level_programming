42e846ef5d8c7b1c1c270e766e2b79e2161037d1
amar-repo <umarahmed072@gmail.com>
2022-09-20 22:02:44 +0000

    Prints elements of array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Prints array of integers
+ * @a: array
+ * @n: number of elements
+ * Return: Returns nothing
+ */
+void print_array(int *a, int n)
+{
+	int let;
+
+	for (let = 0; let < n; let++)
+	{
+		if (let != n - 1)
+		{
+			printf("%d\n, ", a[let]);
+		}
+		else
+		{
+			printf("%d\n, a[let]);
+		}
+		_putchar('\n');
+}
