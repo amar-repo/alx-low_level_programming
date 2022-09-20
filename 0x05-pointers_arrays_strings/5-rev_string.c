0dac77eedca6b2b41d78cadff5600290035774ae
amar-repo <umarahmed072@gmail.com>
2022-09-20 21:24:56 +0000

    Reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string
+ * @s: string
+ * Return: Returns nothing
+ */
+void rev_string(char *s)
+{
+	int i, c, len = _strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		c = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = c;
+	}
+}
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	return (0);
+}
