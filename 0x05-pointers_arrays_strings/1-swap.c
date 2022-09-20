58dc909c833896d266ce9a4a1053d10eeb561650
amar-repo <umarahmed072@gmail.com>
2022-09-20 19:41:14 +0000

    Swaps integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - swap two integers
+ * @a: variable a
+ * @b: variable b
+ * Return: Returns nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
