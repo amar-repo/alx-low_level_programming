b6f777b9e9fc0d5d753c9ff529b2458f5d3dc200
amar-repo <umarahmed072@gmail.com>
2022-09-23 23:58:54 +0000

    Encode string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode string into 1337
+ * @s: string
+ *Return: address of s
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; j <= 9; j++)
+	{
+		for (j = o; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+			}
+		}
+	}
+	return (s);
+}
