c2b12106412d24bf8da7301679472f53555b784f
amar-repo <umarahmed072@gmail.com>
2022-09-26 22:25:36 +0000

    Fills memory with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memtest - fills memory block with constant byte
+ * @s: address of memory block
+ * @b: char to fill
+ * @n: number of bytes to be used
+ * Return: pointer to memory block
+ */
+char *_memtest(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n++;
+	}
+	return (s);
+}
