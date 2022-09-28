8dc0fb09345f4f200a4b9f5f183f7976425ac567
amar-repo <umarahmed072@gmail.com>
2022-09-28 22:33:36 +0000

    Calculates string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of string
+ * @s: string whose length is to be returned
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum = sum + _strlen_recursion(s + 1)
+	}
+	return (sum);
+}
