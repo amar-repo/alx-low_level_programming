c66fc75f6dc7f07ae133f970d842ba4a9b2d3b4a
amar-repo <umarahmed072@gmail.com>
2022-10-03 16:11:10 +0000

    Prints program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: pointer to argument string
  *Return: nothing
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
