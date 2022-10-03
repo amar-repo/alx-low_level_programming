f63decebce2cbd62bf4cb54168f9efcef45523ac
amar-repo <umarahmed072@gmail.com>
2022-10-03 17:39:03 +0000

    Adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Adds positive numbers
+ * @argc: number of arguments passed
+ * @argv: pointer to array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
