fd0938636536f4a84f1a97a0d4666acda98abf53
amar-repo <umarahmed072@gmail.com>
2022-10-03 17:44:08 +0000

    Adds numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,9 +15,10 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
-			if (isdigit(argv[i][j]) == 0)
+			if (isdigit(argv[i][j])) == 0)
 			{
 				puts("Error");
+				return (1);
 			}
 		}
 	}
