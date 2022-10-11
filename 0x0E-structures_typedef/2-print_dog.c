00158cc704cbfcdfb94ef30b0fc81f1cfa38c8f1
amar-repo <umarahmed072@gmail.com>
2022-10-11 12:16:30 -0700

    Prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,47 +1,24 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
- *  * new_dog - creates a new dog
- *   * @name: name of dog
- *    * @age: age of dog
- *     * @owner: owner of dog
- *      *
- *       * Return: pointer to new dog
+ *  * print_dog - prints a struct dog
+ *   * @d: pointer to structure
+ *    *
+ *     * Return: void
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void print_dog(struct dog *d)
 {
-	unsigned int nl, ol, i;
-	dog_t *dog;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
-	for (nl = 0; name[nl]; nl++)
-		;
-	nl++;
-	dog->name = malloc(nl * sizeof(char));
-	if (dog->name == NULL)
+	if (d != NULL)
 	{
-		free(dog);
-		return (NULL);
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+		printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 	}
-	for (i = 0; i < nl; i++)
-		dog->name[i] = name[i];
-	dog->age = age;
-	for (ol = 0; owner[ol]; ol++)
-		;
-	ol++;
-	dog->owner = malloc(ol * sizeof(char));
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
-	for (i = 0; i < ol; i++)
-		dog->owner[i] = owner[i];
-	return (dog);
 }
