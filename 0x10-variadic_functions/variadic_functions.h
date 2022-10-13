4cd77f29e42d055ce8450c292b41779134567f83
amar-repo <umarahmed072@gmail.com>
2022-10-13 13:19:14 -0700

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,8 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
+#include <stdio.h>
 #include <stdarg.h>
-/**
- * struct print - print type with corresponding print functions
- * @t: print type
- * @f: print function
- */
-typedef struct print
-{
-	char *t;
-	void (*f)(va_list);
-} print_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
