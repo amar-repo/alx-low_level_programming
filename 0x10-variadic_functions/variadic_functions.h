499df013b377eef2b2103e4050ae1d4bfe2ae02a
amar-repo <umarahmed072@gmail.com>
2022-10-13 13:44:55 -0700

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -10,4 +10,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print _all(const char * const format, ...);
 
-#endif /* _VARIADIC_FUNCTIONS_H_ */
+#endif /* VARIADIC_FUNCTIONS_H */
