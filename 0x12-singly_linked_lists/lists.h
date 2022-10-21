b6c004b940af84032d8ba5a2dcba702c49e34c8f
amar-repo <umarahmed072@gmail.com>
2022-10-21 16:09:15 -0700

    Header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct list_s - A singly liked list
  * @str: string - malloc'ed string
@@ -17,26 +21,10 @@ typedef struct list_s
 } list_t;
 
 int_strlen(const char *s);
-size_t print_list (const list_t *h);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
-
-
-_putchar
-
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
- */
-int _putchar(char c)
-{
-	return(write(1, &c, 1));
-}
+#endif /* _LISTS_H */
